756656279810f38f3f6159a52899dfdf228ae989
Michael <michaelpereira408@gmail.com>
2025-05-17 17:21:01 -0300

    refactor: refactoring create customer

--- src/modules/customer/domain/application/use-cases/create-customer.use-case.ts
+++ src/modules/customer/domain/application/use-cases/create-customer.use-case.ts
@@ -1,25 +1,26 @@
 import { CustomerRepository } from "src/modules/customer/domain/repositories/customer.repository";
-import { CreateCustomerDTO } from "../dtos/create-customer.dto";
-import { Customer } from "../../../entities/customer.entity";
+
 import { ConflictException, Inject } from "@nestjs/common";
+import { Customer } from "../../entities/customer.entity";
+import { CreateCustomerDTO } from "../dtos/create-customer.dto";
 
 export class CreateCustomerUseCase {
   constructor(
     @Inject(CustomerRepository)
     private readonly customerRepository: CustomerRepository,
-  ) {}
+  ) { }
 
   async execute(data: CreateCustomerDTO): Promise<Customer> {
 
     const customerAlreadyExists = await this.customerRepository.findByEmail(data.email);
-    
+
     if (customerAlreadyExists) {
       throw new ConflictException('Customer already exists');
     }
 
-    const customer =  await this.customerRepository.create(data);
+    const customer = await this.customerRepository.create(data);
 
     return customer;
-  
+
   }
 }
