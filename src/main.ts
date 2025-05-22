117008165515d9bf2c69d078242230811b2e676e
Michael <michaelpereira408@gmail.com>
2025-05-21 22:11:42 -0300

    chore: add logger and env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+import { env } from './shared/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+  app.useLogger(logger);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Agendamentos')
@@ -13,6 +18,9 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(env.PORT).then(async () => {
+    logger.log(`Application is running on: ${(await app.getUrl())}`);
+    logger.log(`Swagger is running on: ${await app.getUrl()}/api`);
+  });
 }
 bootstrap();
