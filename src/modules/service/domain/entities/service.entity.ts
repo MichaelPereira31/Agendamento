f6c2ae9de7eca1e2de2c8846d3a721d0ef782564
Michael <michaelpereira408@gmail.com>
2025-05-21 21:46:31 -0300

    feat: add entity service

---
+++ src/modules/service/domain/entities/service.entity.ts
@@ -0,0 +1,9 @@
+export class Service {
+  public id: string;
+  public name: string;
+  public description: string;
+  public price: number;
+  public durationMinutes: number; 
+  public createdAt: Date;
+  public updatedAt: Date;
+}
